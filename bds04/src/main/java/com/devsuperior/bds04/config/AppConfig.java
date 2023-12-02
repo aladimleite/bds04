380aea8ba780a5c55a6eba2647057012a0986963
Aladim Santana <aladimleite@gmail.com>
2023-12-02 18:52:07 -0300

    bds04

---
+++ bds04/src/main/java/com/devsuperior/bds04/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds04.config;
+
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+    
+    //'jwt.secret' is an unknown property.
+    //@Value("${jwt.secret}") //aula 03-24 variável definida na classe application.properties
+    //private String jwtSecret;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    } 
+    
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+	    JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+	    tokenConverter.setSigningKey("MY-JWT-SECRET"); //aula 03-24 0504
+        //tokenConverter.setSigningKey(jwtSecret);
+	    return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }      
+}
---
+++ bds04/src/main/java/com/devsuperior/bds04/config/AuthorizationServerConfig.java
@@ -0,0 +1,82 @@
+package com.devsuperior.bds04.config;
+
+/*import java.util.Arrays;*/
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+/*import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;*/
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/*import com.devsuperior.dscatalog.components.JwtTokenEnhancer;*/
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    //aula 03-24 0509
+    @Value("${security.oauth2.client.client-id}") //aula 03-24 variável definida na classe application.properties
+    private String clientId;
+
+    @Value("${security.oauth2.client.client-secret}")
+    private String clientSecret;
+    
+    //'jwt.duration' is an unknown property.
+    //@Value("${jwt.duration}")
+    //private Integer jwtDuration;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    //aula 0325 07m38s
+    /*@Autowired
+    private JwtTokenEnhancer tokenEnhancer;*/
+    
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+        .withClient(clientId)
+        .secret(passwordEncoder.encode(clientSecret))
+        .scopes("read","write")
+        .authorizedGrantTypes("password")
+        .accessTokenValiditySeconds(86400); //24h = 86400segundos        
+        //.accessTokenValiditySeconds(jwtDuration);
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        //aula 0325 08m21s
+        /*TokenEnhancerChain chain = new TokenEnhancerChain();
+        chain.setTokenEnhancers(Arrays.asList(accessTokenConverter, tokenEnhancer));*/
+
+        endpoints.authenticationManager(authenticationManager)
+        .tokenStore(tokenStore)
+        .accessTokenConverter(accessTokenConverter);
+        /*.tokenEnhancer(chain); //aula 0325 09m17s*/
+    }
+    
+    
+}
